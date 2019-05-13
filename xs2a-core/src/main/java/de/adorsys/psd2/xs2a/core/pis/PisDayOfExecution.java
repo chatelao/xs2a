05288d5b26e2b5270e4f62926ca59e82b371f29a
Andrii Murashkin <amu@adorsys.com.ua>
2019-05-13 14:15:09 +0300

    #809 removed pmd error;

--- xs2a-core/src/main/java/de/adorsys/psd2/xs2a/core/pis/PisDayOfExecution.java
+++ xs2a-core/src/main/java/de/adorsys/psd2/xs2a/core/pis/PisDayOfExecution.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 /**
  * Day of execution as string in the form DD.  This string consists always of two characters.  31 is ultimo of the
